d81d1655d2f1c88d56b82853f9ae4920ab4cdc2e
8778saquib <saquib.nawaz009@gmail.com>
2022-05-27 22:46:42 +0530

    peek and pop

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -7,24 +7,60 @@ namespace StackandQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues program");
-
-            Console.WriteLine("\n1. Create Stack of 56->30->70");
-            Console.Write("Enter your choice : ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
+            int choice;
+            do
             {
-                case 1:
-                    Stack s1 = new Stack();
-                    s1.Push(70);
-                    s1.Push(30);
-                    s1.Push(56);
-                    s1.display();
-                    break;
-                default:
-                    Console.WriteLine("Please choose correct option from the list");
-                    break;
-            }
+
+                Console.WriteLine("\n1. Create Stack of 56->30->70");
+                Console.WriteLine("2. Peek and Pop till the stack is empty");
+                Console.Write("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Stack s1 = new Stack();
+                        s1.Push(70);
+                        s1.Push(30);
+                        s1.Push(56);
+                        s1.display();
+                        break;
+                    case 2:
+                        Stack s2 = new Stack();
+                        s2.Push(70);
+                        s2.Push(30);
+                        s2.Push(56);
+                        s2.display();
+
+                        //Peek and Pop 1st
+                        s2.Peek();
+                        s2.Pop();
+                        Console.Write("After pop operation ");
+                        s2.display();
+
+                        //Peek and Pop 2nd
+                        s2.Peek();
+                        s2.Pop();
+                        Console.Write("After pop operation ");
+                        s2.display();
+
+                        //Peek and Pop 3rd
+                        s2.Peek();
+                        s2.Pop();
+                        Console.Write("After pop operation ");
+                        s2.display();
+                        break;
+                    default:
+                        Console.WriteLine("Please choose correct option from the list");
+                        break;
+                }
+                
+
+
+
+            } while (choice != 0);
+
         }
+
     }
 }
